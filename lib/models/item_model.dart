eb9e5f46ffc8c7bee684ede08b7e42f95597ee82
Filip Szymczuk <fszymczuk@gmail.com>
2022-09-05 21:44:47 +0200

    map

---
+++ lib/models/item_model.dart
@@ -0,0 +1,12 @@
+class ItemModel {
+  ItemModel(
+      {required this.id,
+      required this.title,
+      required this.ImageURL,
+      required this.relaseDate});
+
+  final String id;
+  final String title;
+  final String ImageURL;
+  final DateTime relaseDate;
+}
